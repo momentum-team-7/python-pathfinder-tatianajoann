72444b0404c0771e5d41c245fb34566a5dc8e00e
tatiana perez <tjperez@ncsu.edu>
2021-02-20 19:35:57 -0500

    can draw a straight line

--- pathfinder.py
+++ pathfinder.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 # 1: Read the data from `elevation_small.txt` into an appropriate data structure to get the elevation data.
 # This file is made up of multiple lines. Each line has a list of numbers representing elevation in meters.
 # The elevation is the maximum elevation for a 90m x 90m square.
@@ -19,7 +19,6 @@ with open("elevation_small.txt", "r") as file:
     height = len(coordinates)
     width = len(coordinates[0])
 # defining dimensions
-
     min_el = int(min(map(min, coordinates)))
     max_el = int(max(map(max, coordinates)))  # finding min and max elevations
     new_coordinates = []  # making a new list
@@ -38,14 +37,40 @@ with open("elevation_small.txt", "r") as file:
     img = Image.new('RGB', (width, height))
     for x in range(width):  # this will iterate through the range of the width
         for y in range(height):  # this will iterate through the range of the height
-
             # get little list with position/index of x from the big list
             value_list = new_coordinates[x]
-
             # get color value with position/index of y from the little list
             color_value = value_list[y]
-
             # use x and y for the position of the pixel. and get the rgb value from the variable above
             img.putpixel((x, y), color_value)
 
     img.save('pil_read.png')
+
+# need to find optimal path.
+# need to use elevation data (coordinates) not new one w rgb
+# neon green rgb: 127, 255, 0. maybe change the color of pixel to this? idk.
+# separate function for doing the line vs finding the path?
+
+
+short_path = []
+
+
+def draw_path(thing_to_draw):
+    img.putpixel(thing_to_draw, (127, 255, 0))
+
+
+def find_short_path():
+    x = 0
+    # starting_pos = coordinates[300][x]
+    # next_position = None
+    for coordinate in coordinates:
+        draw_path((x, 300))
+        x = x + 1
+    img.save('pil_read.png')
+
+
+find_short_path()
+
+# draw = ImageDraw.Draw(im)
+# draw.line((100, 200, 150, 300), fill=128)
+# im.show()
